f957df806eaacac0723326ae2964f3ea5784d64a
ved <ved28592@gmail.com>
2019-08-26 19:10:13 -0400

    changed model and extractor classes

--- classes.py
+++ classes.py
@@ -1,99 +1,238 @@
 import torch
-
 from torch import nn
-from torch.utils.data import Dataset, DataLoader
-from torchvision import models, transforms as T
+
+from torch.utils.data import Dataset  # , DataLoader
+from torchvision import models  # , transforms as T
+# from torchvision.utils import save_image
 
 
-# class vgg_hook(models.VGG):
 class Extractor(nn.Module):
 
-    def __init__(self, content, style, content_wt=1.0, style_wt=0.2):
+    def __init__(self, content, style, content_wt=1.0, style_wt=0.2, total_wt=1.0, device=None):
 
         super(Extractor, self).__init__()
 
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        if device:
+            self.device = device
+        else:
+            self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+        # self.base = models.vgg16(pretrained=True).features.to(self.device)
         self.base = models.vgg19(pretrained=True).features.to(self.device)
         self.base.eval()
-        # self.base.zero_grad()
 
         self.ST_WEIGHT = content_wt
         self.CT_WEIGHT = style_wt
-
-        self.gram_list_ip = []
-        self.gram_list_st = []
-        self.ct_feat = None
-        self.ip_feat = None
-
-        self.ct_loss = nn.MSELoss()
-        self.st_loss = nn.MSELoss()
-
-        self.ct_losses = 0
-        self.st_losses = 0
+        self.TOT_WEIGHT = total_wt
 
         # [2, 7, 14, 23, 32]
-        # for i in ['2', '7', '14', '23', '32']:
-        for i in ['3', '8', '24', '33']:
-            self.base._modules[i].register_forward_hook(hook=self.style_hook)
+        # for i in ['3', '8', '24', '33']:
+        # for i in ['2', '7', '23', '32']:
+        # for i in ['3', '8', '22', '29']:
+        #     self.base._modules[i].register_forward_hook(hook=self.style_hook)
 
         # [14]
-        self.base._modules['15'].register_forward_hook(hook=self.c_and_s_hook)
+        # self.base._modules['15'].register_forward_hook(hook=self.c_and_s_hook)
+        # self.base._modules['14'].register_forward_hook(hook=self.c_and_s_hook)
 
         # self.base.register_forward_hook(hook=self.end_game)
 
-        self.pass_ = "C"
-        self.base(content)
+        modules = list(self.base.children())
 
-        self.pass_ = "S"
-        self.base(style)
+        self.layer1 = nn.Sequential(*modules[:4])
+        self.layer2 = nn.Sequential(*modules[4:9])
+        self.layer3 = nn.Sequential(*modules[9:18])
+        self.layer3_5 = nn.Sequential(*modules[18:23])      # Content layer
+        self.layer4 = nn.Sequential(*modules[23:27])
+        self.layer5 = nn.Sequential(*modules[27:36])
+        self.end_layer = nn.Sequential(*modules[36:])
 
-    # def forward(self, x, content, style):
+        # def forward(self, x, content, style):
+
+        y = self.layer1(style)
+        self.st_loss_1 = StyleLoss(y)
+
+        y = self.layer2(y)
+        self.st_loss_2 = StyleLoss(y)
+
+        y = self.layer3(y)
+        self.st_loss_3 = StyleLoss(y)
+
+        y = self.layer4(self.layer3_5(y))
+        self.st_loss_4 = StyleLoss(y)
+
+        y = self.layer5(y)
+        self.st_loss_5 = StyleLoss(y)
+
+        y = self.layer3_5(self.layer3(self.layer2(self.layer1(content))))
+        self.ct_loss = ContentLoss(y)
+
+        for param in self.parameters():
+            param.requires_grad = False
+
     def forward(self, x):
 
-        self.pass_ = "X"
-        self.base(x)
+        y = self.layer1(x)
+        style_loss = self.st_loss_1(y)
 
-        # self.pass_ = "C"
-        # self.base(content)
-        #
-        # self.pass_ = "S"
-        # self.base(style)
+        y = self.layer2(y)
+        style_loss += self.st_loss_2(y)
 
-    def c_and_s_hook(self, module, x, output):
-        """
-        Forward hook for storing output for content, style and generated images.
+        y = self.layer3(y)
+        style_loss += self.st_loss_3(y)
 
-        :param module:  Module object for hook
-        :param x:       Input to the module
-        :param output:  Output of the module
-        :return:        None
-        """
+        y = self.layer3_5(y)
+        content_loss = self.ct_loss(y)
 
-        if self.pass_ == "C":
-            self.ct_feat = output
-        elif self.pass_ == "S":
-            self.gram_list_st.append(output)
-        elif self.pass_ == "X":
-            self.ip_feat = output
-            self.gram_list_ip.append(output)
+        y = self.layer4(y)
+        style_loss += self.st_loss_4(y)
 
-    def style_hook(self, module, x, output):
-        """
-        Forward hook for storing output for style and generated images.
+        y = self.layer5(y)
+        style_loss += self.st_loss_5(y)
 
-        :param module:  Module object for hook
-        :param x:       Input to the module
-        :param output:  Output of the module
-        :return:        None
-        """
-        # print(self.__class__.__name__)
-        # print(self2.__class__.__name__)
+        y = self.end_layer(y)
 
-        if self.pass_ == "S":
-            self.gram_list_st.append(output)
-        elif self.pass_ == "X":
-            self.gram_list_ip.append(output)
+        return style_loss * self.ST_WEIGHT, content_loss * self.CT_WEIGHT, y
+
+
+class StyleTransfer(nn.Module):
+
+    def __init__(self, intrim_layers=6, resnet=False, bias=False):
+
+        super(StyleTransfer, self).__init__()
+
+        self.resnet = resnet
+
+        self.init_layer = nn.Sequential(nn.Conv2d(in_channels=3,
+                                                  out_channels=8,
+                                                  kernel_size=3,
+                                                  stride=1,
+                                                  padding=1,
+                                                  bias=bias),
+                                        nn.ReLU(),
+                                        nn.BatchNorm2d(8),
+                                        # nn.ReflectionPad2d(1),
+                                        nn.Conv2d(in_channels=8,
+                                                  out_channels=16,
+                                                  kernel_size=3,
+                                                  stride=1,
+                                                  padding=1,
+                                                  bias=bias),
+                                        nn.ReLU(),
+                                        nn.BatchNorm2d(16),
+                                        # nn.ReflectionPad2d(1),
+                                        nn.Conv2d(in_channels=16,
+                                                  out_channels=32,
+                                                  kernel_size=3,
+                                                  stride=1,
+                                                  padding=1,
+                                                  bias=bias),
+                                        nn.ReLU(),
+                                        nn.BatchNorm2d(32)
+                                        )
+
+        layers = []
+
+        for i in range(intrim_layers):
+            layers.append(Residual(channels=32,
+                                   k_size=3,
+                                   resnet=resnet,
+                                   bias=bias)
+                          )
+        #     layers.append(nn.ReLU())
+
+        self.layers = nn.Sequential(*layers)
+
+        self.final_layer = nn.Sequential(nn.Conv2d(in_channels=32,
+                                                   out_channels=8,
+                                                   kernel_size=3,
+                                                   stride=1,
+                                                   padding=1,
+                                                   bias=bias),
+                                         nn.ReLU(),
+                                         nn.BatchNorm2d(8),
+                                         # nn.ReflectionPad2d(1),
+                                         nn.Conv2d(in_channels=8,
+                                                   out_channels=3,
+                                                   kernel_size=3,
+                                                   stride=1,
+                                                   padding=1,
+                                                   bias=bias),
+                                         nn.ReLU(),
+                                         nn.BatchNorm2d(3)
+                                         )
+
+    def forward(self, x):
+        jump = self.init_layer(x)
+        if self.resnet:
+            y = self.layers(jump) + jump
+        else:
+            y = self.layers(jump)
+        out = self.final_layer(y)
+
+        return out
+
+
+class Residual(nn.Module):
+
+    def __init__(self, channels, k_size=3, stride=1, resnet=False, bias=False):
+        super(Residual, self).__init__()
+
+        self.resnet = resnet
+
+        self.conv = nn.Sequential(nn.Conv2d(in_channels=channels,
+                                            out_channels=channels,
+                                            kernel_size=k_size,
+                                            stride=stride,
+                                            padding=k_size // 2,
+                                            bias=bias),
+                                  nn.ReLU(),
+                                  # nn.ReflectionPad2d(k_size // 2),
+                                  # nn.Conv2d(in_channels=channels,
+                                  #           out_channels=channels,
+                                  #           kernel_size=k_size,
+                                  #           stride=stride,
+                                  #           bias=bias),
+                                  # nn.ReLU(),
+                                  nn.BatchNorm2d(channels)
+                                  )
+
+    def forward(self, x):
+        if self.resnet:
+            # return self.conv(x) + self.conv2(x)
+            return self.conv(x) + x
+        else:
+            return self.conv(x)
+
+
+class ContentLoss(nn.Module):
+    def __init__(self, content):
+        super(ContentLoss, self).__init__()
+
+        self.target = content
+
+        self.crit = nn.MSELoss()
+
+        pass
+
+    def forward(self, x):
+        return self.crit(x, self.target)
+
+
+class StyleLoss(nn.Module):
+    def __init__(self, style):
+        super(StyleLoss, self).__init__()
+
+        self.target = self.gram_matrix(style)
+        # self.target = style
+
+        self.crit = nn.MSELoss()
+
+        pass
+
+    def forward(self, x):
+        return self.crit(self.gram_matrix(x), self.target)
+        # return self.crit(x, self.target)
 
     def gram_matrix(self, tensor):
         """
@@ -109,92 +248,3 @@ class Extractor(nn.Module):
         gram = torch.mm(features, features.t())
 
         return gram.div(a * b * c * d)
-
-    def end_game(self):
-
-        self.ct_losses = self.ct_loss(self.ip_feat, self.ct_feat) * self.CT_WEIGHT
-
-        for ip, st in zip(self.gram_list_ip, self.gram_list_st):
-            self.st_losses += self.st_loss(self.gram_matrix(ip), self.gram_matrix(st)) * self.ST_WEIGHT
-
-        self.ip_feat = None
-        self.gram_list_ip.clear()
-
-        return self.ct_losses + self.st_losses
-
-
-class StyleTransfer(nn.Module):
-
-    def __init__(self, bias=False):
-
-        super(StyleTransfer, self).__init__()
-
-        self.init_layer = Conv(in_ch=3,
-                               out_ch=8,
-                               k_size=3,
-                               bias=bias)
-
-        layers = []
-
-        for i in range(3):
-            layers.append(Conv(in_ch=8 * 2 ** i,
-                               out_ch=16 * 2 ** i,
-                               k_size=3,
-                               bias=bias)
-                          )
-            layers.append(nn.ReLU())
-            layers.append(nn.BatchNorm2d(16 * 2 ** i))
-
-        for i in range(2, -1, -1):
-            layers.append(Conv(in_ch=16 * 2 ** i,
-                               out_ch=8 * 2 ** i,
-                               k_size=3,
-                               bias=bias)
-                          )
-            layers.append(nn.ReLU())
-            layers.append(nn.BatchNorm2d(8 * 2 ** i))
-
-        self.layers = nn.Sequential(*layers)
-
-        self.final_layer = Conv(in_ch=8,
-                                out_ch=3,
-                                k_size=5,
-                                bias=bias)
-
-        # TODO: define a model for actual Style Transfer
-
-    def forward(self, x):
-        y = self.init_layer(x)
-        y = self.layers(y)
-        y = self.final_layer(y)
-        return y
-
-
-class Conv(nn.Module):
-
-    def __init__(self, in_ch, out_ch, k_size=3, stride=1, bias=False):
-        super(Conv, self).__init__()
-
-        self.conv = nn.Sequential(nn.ReflectionPad2d(k_size // 2),
-                                  nn.Conv2d(in_channels=in_ch,
-                                            out_channels=out_ch,
-                                            kernel_size=k_size,
-                                            stride=stride,
-                                            bias=bias),
-                                  nn.ReLU()
-                                  )
-
-    def forward(self, x):
-        return self.conv(x)
-
-
-class STDataSet(Dataset):
-
-    def __init__(self):
-        super(STDataSet, self).__init__()
-
-    def __getitem__(self, item):
-        pass
-
-    def __len__(self):
-        return 0
--- main.py
+++ main.py
@@ -9,11 +9,13 @@
 #  repeat style image to match batch size.
 import torch
 
-from torch import cuda, optim
+from torch import cuda, optim, nn
 from torchvision import models, transforms as T
 from torchvision.utils import save_image
 from torchsummary import summary
 from PIL import Image
+from support.invert import Invert
+# from torchviz import make_dot, make_dot_from_trace
 
 import argparse as ap
 
@@ -22,67 +24,129 @@ from classes import Extractor, StyleTransfer
 
 def main(args):
 
-    device = torch.device("cuda:0" if cuda.is_available() else "cpu")
+    device = torch.device("cuda:0" if cuda.is_available() and not args.cpu else "cpu")
+    # device = torch.device("cpu")
 
-    model = StyleTransfer().to(device)
+    model = StyleTransfer(intrim_layers=args.layers,
+                          resnet=True,
+                          bias=True,
+                          ).to(device)
     model.train()
 
     optimizer = optim.Adam(params=model.parameters(),
                            lr=args.lr,
-                           weight_decay=0,
+                           weight_decay=args.lamb,
                            amsgrad=False
                            )
 
-    # summary(model, (3, 500, 500), 1)
+    # summary(model.cuda(), (3, 480, 480), 1)
+    # exit(0)
 
-    test_tr = T.Compose([T.CenterCrop(1280),
-                         T.Resize(500),
-                         T.ToTensor()
-                         ])
+    transform = T.Compose([T.Resize(480),
+                           T.CenterCrop(480),
+                           T.ToTensor(),
+                           # T.Normalize([0.4, 0.4, 0.4], [0.2, 0.2, 0.2]),
+                           ])
 
-    style_tr = T.Compose([T.CenterCrop(895),
-                          T.Resize(500),
-                          T.ToTensor()
-                          ])
+    end_tr = T.Compose([T.ToPILImage(mode="RGB"),
+                        Invert(),
+                        T.ToTensor()
+                        ])
 
-    content_image = Image.open("test/test.jpg")
-    content_image = test_tr(content_image).to(device).unsqueeze(dim=0)
-    # content_image = torch.zeros(2).unsqueeze(dim=0)
-    style_image = Image.open("style/abstract2.jpg")
-    style_image = style_tr(style_image).to(device).unsqueeze(dim=0)
-    # TODO: accomodate batch size
+    content_image = Image.open("test/girl.jpg")
+    content_image = transform(content_image).to(device).unsqueeze(dim=0)
+
+    # style_image = Image.open("style/abstract2.jpg")
+    style_image = Image.open("style/mosaic.jpg")
+    style_image = transform(style_image).to(device).unsqueeze(dim=0)
 
     ex = Extractor(content=content_image,
                    style=style_image,
-                   content_wt=0.1,
-                   style_wt=2.0
+                   content_wt=args.content_weight,
+                   style_wt=args.style_weight,
+                   total_wt=args.total_weight,
+                   device=device
                    ).to(device)
-    ex.train()
 
-    for i in range(args.instances):
+    total_crit = nn.MSELoss()
+
+    # summary(ex.cuda(), (3, 480, 480), 1)
+    # exit(0)
+
+    ex.train(False)
+
+    # model.ex = ex
+
+    for i in range(args.iterations):
+
+        # content_image = Variable(content_image, requires_grad=True)
 
         optimizer.zero_grad()
-
         st_im = model(content_image)
 
-        save_image(tensor=st_im,
-                   filename="output/ST_{0}.jpg".format(i),
-                   nrow=1,
-                   normalize=False
-                   )
+        # min_v = torch.min(st_im)
+        # range_v = torch.max(st_im) - min_v
+        # if range_v > 0:
+        #     normalised = (st_im - min_v) / range_v
+        # else:
+        #     normalised = torch.zeros(st_im.size())
+        # ex(normalised)
 
-        ex(st_im)
+        # loss_1, loss_2 = ex(st_im)
+        loss_1, loss_2, feat = ex(st_im)
 
-        losses = ex.end_game()
+        # feat2 = ex.base(content_image)
 
-        print("Iteration: {0}\tLosses: {1}".format(i+1, losses.item()))
+        # loss_3 = total_crit(feat, feat2) * args.total_weight
 
-        losses.backward(retain_graph=True)
+        losses = loss_1 + loss_2
+        # losses = loss_1 + loss_2 + loss_3
+
+        # ex(st_im)
+        # losses = ex.end_game()
+
         # losses.backward()
+        losses.backward(retain_graph=True)
+        # https://jdhao.github.io/2017/11/12/pytorch-computation-graph/
+        # This link may hold the explanation for failed graphs.
 
         optimizer.step()
+        # optimizer.step(closure)
 
         cuda.empty_cache()
+        # st_im.clamp_(0, 1)
+
+        # min_v = torch.min(st_im)
+        # range_v = torch.max(st_im) - min_v
+        # if range_v > 0:
+        #     normalised = (st_im - min_v) / range_v
+        # else:
+        #     normalised = torch.zeros(st_im.size())
+
+        # inv = end_tr(normalised.squeeze(0)).unsqueeze(dim=0)
+        inv = end_tr(st_im.squeeze(0).cpu()).unsqueeze(dim=0).to(device)
+
+        save_image(tensor=torch.cat([content_image, st_im, style_image]),
+                   filename="output/ST_{0}.jpg".format(i),
+                   nrow=3,
+                   normalize=True,
+                   scale_each=True
+                   )
+
+        # print("Iteration: {0}\tLosses ->\tCT: {1}\tST: {2}".format(i+1, ct_l.item(), st_l.item()))
+        print("Iteration: {0}\tLosses: {1}".format(i+1, losses.item()))
+        # ex.clear()
+
+    # test_im = model(content_image)
+    # ex = None
+    # model.eval()
+    # inv = end_tr(test_im.squeeze(0)).unsqueeze(dim=0)
+    #
+    # save_image(tensor=torch.cat([content_image, inv, style_image]),
+    #            filename="output/ST_Test.jpg",
+    #            nrow=3,
+    #            normalize=False
+    #            )
 
 
 if __name__ == '__main__':
@@ -93,12 +157,45 @@ if __name__ == '__main__':
                         type=float,
                         default=0.001,
                         help="Learning rate")
+
     parser.add_argument("-i",
-                        "--instances",
+                        "--iterations",
                         type=int,
                         default=10,
-                        help="Number of Instances")
+                        help="Number of Iterations (Default 10)")
+
+    parser.add_argument("--style-weight",
+                        type=float,
+                        default=1e8,
+                        help="Style weight (1e8)")
+
+    parser.add_argument("--content-weight",
+                        type=float,
+                        default=1e4,
+                        help="Content weight (1e4)")
+
+    parser.add_argument("--total-weight",
+                        type=float,
+                        default=1.0,
+                        help="Total loss weight (1)")
+
+    parser.add_argument("--lamb",
+                        type=float,
+                        default=0.0,
+                        help="Weight decay for optimizer")
+
+    parser.add_argument("--cpu",
+                        action="store_true",
+                        help="Use CPU for training instead of GPU")
+
+    parser.add_argument("-l",
+                        "--layers",
+                        type=int,
+                        default=5,
+                        help="Number of Layers in intermediate section")
 
     arg = parser.parse_args()
 
     main(arg)
+
+#     https://github.com/pytorch/examples/blob/master/fast_neural_style/neural_style/neural_style.py
